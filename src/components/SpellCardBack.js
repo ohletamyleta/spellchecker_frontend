2cc61a00423cfd6491d15cb7422fd6872b724fa9
ohletamyleta <ohletamyleta@gmail.com>
2021-05-11 12:57:23 -0400

    build spell cards and container

--- src/components/SpellCardBack.js
+++ src/components/SpellCardBack.js
@@ -1,3 +1,22 @@
 //back view of flippable spell card
 //will contain stats not included on front
 //buttons: add to spellbook, flip
+import React from 'react';
+
+function SpellCardBack(props) {
+  return (
+    <div className="Card-Back">
+      <h2>{props.name}</h2>
+      Range: {props.range}
+      Components: {props.components}
+      Duration: {props.duration}
+      Concentration: {props.concentration}
+      Casting Time: {props.casting_time}
+      School: {props.school}
+      Archetype: {props.archetype}
+      Circles: {props.circles}
+    </div>
+  );
+}
+
+export default SpellCardBack;
--- src/containers/SpellContainer.js
+++ src/containers/SpellContainer.js
@@ -1,12 +1,77 @@
 import React, { Component } from 'react';
+import SpellCardFront from '../components/SpellCardFront';
+import SpellCardBack from '../components/SpellCardBack';
 
 class SpellContainer extends Component {
+  state = {
+    spells: [],
+    casterClass: "",
+  }
+
+  makeSpellCardsFront() {
+    let spellList = this.state.spells;
+
+    if (this.state.casterClass) {
+      spellList = this.state.spells.filter((spell) =>
+        spell.dnd_class
+          .toUpperCase()
+          .includes(this.state.casterClass.toUpperCase())
+      );
+    }
+
+    return spellList.map((spell) => (<SpellCardFront name={spell.name} level={spell.level} dnd_class={spell.dnd_class} desc={spell.desc} higher_level={spell.higher_level}/>
+    ));
+  }
+
+  makeSpellCardsBack() {
+    let spellList = this.state.spells;
+    // console.log(this.state.spells);
+    if (this.state.casterClass) {
+      spellList = this.state.spells.filter((spell) =>
+        spell.dnd_class
+          .toUpperCase()
+          .includes(this.state.casterClass.toUpperCase())
+      );
+    }
+
+    return spellList.map((spell) => (
+      <SpellCardBack
+        name={spell.name}
+        range={spell.range}
+        components={spell.components}
+        duration={spell.duration}
+        concentration={spell.concentration}
+        casting_time={spell.casting_time}
+        School={spell.school}
+        archetype={spell.archetype}
+        circles={spell.circles}
+      />
+    ));
+  }
+
+  componentDidMount() {
+    fetch("https://api.open5e.com/spells/?limit=400")
+      .then((response) => response.json())
+      .then((data) => {
+        // console.log(data.results);
+        this.setState({
+          spells: data.results,
+        });
+        console.log(this.state);
+      });
+  }
 
   render() {
-return (
-  <h2>SpellContainer goes here</h2>
-)
-}
+    return (
+      <div id="spell-container">
+        <div>
+          <button>Sort By Caster Class Placeholder</button>
+        </div>
+          {this.makeSpellCardsFront()}
+          {this.makeSpellCardsBack()}
+      </div>
+    )
+  }
 }
 
 export default SpellContainer;
