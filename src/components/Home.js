812b23f66bd7667b05d4252559aedec626328ab7
ohletamyleta <ohletamyleta@gmail.com>
2021-05-11 14:17:53 -0400

    add react router

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,12 @@
 //I'm going to have a blurb about the app
 //and render the login and signup buttons
 //and a cool background image
+import React from "react";
+
+class Home extends React.Component {
+  render() {
+    return <h1>Home!</h1>;
+  }
+}
+
+export default Home; 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,75 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
-import "../App.css";
+const link = {
+  width: "100px",
+  padding: "12px",
+  margin: "0 6px 6px",
+  background: "black",
+  textDecoration: "none",
+  color: "white",
+}
 
-const NavBar = () => {
-  return (
-  <h4>I am a navbar!</h4>
-  )}
-
-
-
-
+class NavBar extends React.Component {
+  render() {
+    return (
+      <div>
+        <NavLink
+          to="/"
+          /* set exact so it knows to only set activeStyle when route is deeply equal to link */
+          exact
+          /* add styling to Navlink */
+          style={link}
+          /* add prop for activeStyle */
+          activeStyle={{
+            background: "darkblue",
+          }}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/login"
+          exact
+          style={link}
+          activeStyle={{
+            background: "darkblue",
+          }}
+        >
+          Login
+        </NavLink>
+        <NavLink
+          to="/signup"
+          exact
+          style={link}
+          activeStyle={{
+            background: "darkblue",
+          }}
+        >
+          Signup
+        </NavLink>
+        <NavLink
+          to="/spells"
+          exact
+          style={link}
+          activeStyle={{
+            background: "darkblue",
+          }}
+        >
+          Spells
+        </NavLink>
+        <NavLink
+          to="/spellbook"
+          exact
+          style={link}
+          activeStyle={{
+            background: "darkblue",
+          }}
+        >
+          Spellbook
+        </NavLink>
+      </div>
+    );
+  }
+}
 
 export default NavBar;
